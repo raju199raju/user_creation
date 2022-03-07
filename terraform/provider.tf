3037befa81d1fd26288fb6c9ccdd8a07078fd5bc
raju.rauniyar <raju.rauniyar@pwc.com>
2022-03-07 16:23:57 +0530

    first commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,14 @@
 provider "azurerm" {
      features {}
-     subscription_id = "729c6f47-0b24-4106-8552-e8373b7fe5fc"
-     client_id = "62489962-f130-479a-a288-4c85d20617f2"
-     client_secret = "ikq7Q~ppQBB44IT0ZZs0fLjUKLFQ1U1AAf.qE"
-     tenant_id = "c4eaa32e-5d01-4773-8371-8037d3b710ab"
-     version = "~>2.0"
+     subscription_id = "060c7910-de04-45a3-9953-ad495ae2be2b"
+     client_id       = "1773045f-c8a6-49d9-841f-40e6b225aecd"
+     client_secret   = "QIQ7Q~Ph7OuxM7EsynP1.wmbUu2XhSGIEnRPB"
+     tenant_id       = "1ceb3aa7-356f-4e03-b604-c324db3e3db8"
+     version         = "~>2.0"
 }
 
 provider "azuread" {
-  client_id = "62489962-f130-479a-a288-4c85d20617f2"
-  client_secret = "ikq7Q~ppQBB44IT0ZZs0fLjUKLFQ1U1AAf.qE"
-  tenant_id = "c4eaa32e-5d01-4773-8371-8037d3b710ab"
+     client_id       = "1773045f-c8a6-49d9-841f-40e6b225aecd"
+     client_secret   = "QIQ7Q~Ph7OuxM7EsynP1.wmbUu2XhSGIEnRPB"
+     tenant_id       = "1ceb3aa7-356f-4e03-b604-c324db3e3db8"
 }
