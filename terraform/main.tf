e2ebb1dc3bdf0c4a70679c6db7bd0e0d920bf6d1
raju.rauniyar <raju.rauniyar@pwc.com>
2022-03-07 17:58:22 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@
 resource "azuread_user" "example" {
   user_principal_name = var.UPN
   display_name        = var.FULL_NAME
-  given_name          = "var.FIRST_NAME"
-  surname             = "var.LAST_NAME"
+  given_name          = var.FIRST_NAME
+  surname             = var.LAST_NAME
   password            = "SecretP@sswd99!"
 }
 
