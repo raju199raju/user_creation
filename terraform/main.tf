571eb3bc8ecfa2f9b3be9b6e932fa89ea0870c4e
raju.rauniyar <raju.rauniyar@pwc.com>
2022-03-08 07:37:06 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,19 +8,3 @@ resource "azuread_user" "example" {
   password            = "SecretP@sswd99!"
 }
 
-// Create AAD Group
-
-data "azuread_client_config" "current" {}
-
-resource "azuread_group" "example" {
-  display_name     = "example"
-  owners           = [data.azuread_client_config.current.object_id]
-  security_enabled = true
-}
-
-// Add user to AAD Group
-
-resource "azuread_group_member" "example" {
-  group_object_id  = azuread_group.example.id
-  member_object_id = azuread_user.example.id
-}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,14 @@
 provider "azurerm" {
      features {}
+     subscription_id = "060c7910-de04-45a3-9953-ad495ae2be2b"
+     client_id       = "1773045f-c8a6-49d9-841f-40e6b225aecd"
+     client_secret   = "QIQ7Q~Ph7OuxM7EsynP1.wmbUu2XhSGIEnRPB"
+     tenant_id       = "1ceb3aa7-356f-4e03-b604-c324db3e3db8"
      version         = "~>2.0"
 }
 
 provider "azuread" {
-     
+     client_id       = "1773045f-c8a6-49d9-841f-40e6b225aecd"
+     client_secret   = "QIQ7Q~Ph7OuxM7EsynP1.wmbUu2XhSGIEnRPB"
+     tenant_id       = "1ceb3aa7-356f-4e03-b604-c324db3e3db8"
 }
