64acce9d8f4d23c0be282025b0a8acf0aede073b
raju.rauniyar <raju.rauniyar@pwc.com>
2022-03-08 08:57:33 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,10 @@ data "azuread_client_config" "current" {}
 resource "azuread_group" "ad_group" {
   display_name     = var.GROUP_NAME
   description      = var.GROUP_DESCRIPTION
+  assignable_to_role = true
   owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
+  prevent_duplicate_names = true
 }
 
 data "azurerm_subscription" "primary" {
