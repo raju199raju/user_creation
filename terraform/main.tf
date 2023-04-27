dc07c5dba7398debd07c2d611931d12080946ecd
raju199raju <50610247+raju199raju@users.noreply.github.com>
2023-04-27 11:09:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,7 @@
 
 data "azuread_client_config" "current" {}
 
-resource "azuread_group" "ad_group" {
-  display_name     = var.GROUP_NAME
-  description      = var.GROUP_DESCRIPTION
-  assignable_to_role = true
-  owners           = [data.azuread_client_config.current.object_id]
-  security_enabled = true
-  prevent_duplicate_names = true
-}
-
-data "azurerm_subscription" "primary" {
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources89"
+  location = "West Europe"
 }
