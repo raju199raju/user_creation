3e7ca666e7a2e317a374141e0f5ad77d91684e87
raju199raju <50610247+raju199raju@users.noreply.github.com>
2023-04-27 10:57:28 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,10 @@
 provider "azurerm" {
      features {}
-     subscription_id = "060c7910-de04-45a3-9953-ad495ae2be2b"
-     client_id       = "1773045f-c8a6-49d9-841f-40e6b225aecd"
-     client_secret   = "QIQ7Q~Ph7OuxM7EsynP1.wmbUu2XhSGIEnRPB"
-     tenant_id       = "1ceb3aa7-356f-4e03-b604-c324db3e3db8"
      version         = "~>2.0"
 }
 
 provider "azuread" {
-     client_id       = "1773045f-c8a6-49d9-841f-40e6b225aecd"
-     client_secret   = "QIQ7Q~Ph7OuxM7EsynP1.wmbUu2XhSGIEnRPB"
+     client_id       = "aa62d15b-a232-418d-83e9-14ccc25e90d0"
+     client_secret   = "SlQ8Q~yqNbrDMpy-2EPK6vXEPipVODsRmBzcFbHA"
      tenant_id       = "1ceb3aa7-356f-4e03-b604-c324db3e3db8"
 }
